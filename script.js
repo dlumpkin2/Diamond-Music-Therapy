92bfe2242f15154d93a102b6224b1b514baf8e65
dlumpkin2 <105502061+dlumpkin2@users.noreply.github.com>
2022-05-13 09:22:25 -0500

    Created using Popcode: https://popcode.org

---
+++ script.js
@@ -0,0 +1,11 @@
+$(".happy-button").dblclick(function() {
+$(".Part-one").hide();
+$(".Part-Two-happy").show();
+});
+
+$(".sad-button").dblclick(function() {
+$(".Part-one").hide();
+$(".Part-Two-sad").show();
+    $(".sadpicture").show();
+    $(".Story-Ending-sadlist").show();
+});
